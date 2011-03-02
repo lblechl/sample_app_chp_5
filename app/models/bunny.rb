05b3c87dee08a8687a73d2451bce88bc6feec602
lblechl <lblechl@stu.matcmadison.edu>
2011-03-01 16:59:06 -0800

    Added attr_accessible in app/models/user.rb

--- app/models/bunny.rb
+++ app/models/bunny.rb
@@ -1,2 +1,15 @@
+# == Schema Information
+# Schema version: 20110302000103
+#
+# Table name: bunnies
+#
+#  id           :integer         not null, primary key
+#  name         :string(255)
+#  fluffly_tail :boolean
+#  hop_distance :integer
+#  created_at   :datetime
+#  updated_at   :datetime
+#
+
 class Bunny < ActiveRecord::Base
 end
